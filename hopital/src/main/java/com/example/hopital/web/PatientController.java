b3c906e16960e0892ab0a1e2b0fc71b310e734a3
SanaeHelen <yousfisanae3@gmail.com>
2024-04-22 22:59:36 +0100

    second push

--- hopital/src/main/java/com/example/hopital/web/PatientController.java
+++ hopital/src/main/java/com/example/hopital/web/PatientController.java
@@ -4,9 +4,12 @@ package com.example.hopital.web;
 import com.example.hopital.entities.Patient;
 import com.example.hopital.repositories.PatientRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,9 +18,13 @@ import java.util.List;
 public class PatientController {
     private PatientRepository patientRepository;
     @GetMapping("/index")
-    public String index(Model model){
-        List<Patient> patientList=patientRepository.findAll();
-        model.addAttribute("ListPatients",patientList);
+    public String index(Model model,
+                        @RequestParam(name = "page",defaultValue = "0") int p,
+                        @RequestParam(name = "size",defaultValue = "4") int s){
+        Page<Patient> pagePatients=patientRepository.findAll(PageRequest.of(p,s));
+        model.addAttribute("ListPatients",pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage",p);
         return "patients";
     }
 }
